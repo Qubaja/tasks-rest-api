bbf133f2ed1d323c4312fe041cfccf57eaa150d4
Mohammed Qubaja <m.qubaja03@gmail.com>
2026-09-09 07:41:56 +0200

    first commit

---
+++ src/main/java/com/example/lerningSpring/LerningSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.lerningSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LerningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LerningSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lerningSpring/LerningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lerningSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LerningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
